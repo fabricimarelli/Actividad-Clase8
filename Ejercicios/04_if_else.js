484cd561a3202408e2f52c3afa4a6142de9562e7
= <fabricimarelli@gmail.com>
2022-05-25 11:09:12 -0300

    ejercicio 4

---
+++ Ejercicios/04_if_else.js
@@ -0,0 +1,33 @@
+/*4. If Else
+
+Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”. [utilizar alert() ]
+Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Menor” si la edad es menor a 18
+“Mayor” si la edad es mayor o igual a 18 Y menor que 65
+“Jubilado” si la edad es mayor o igual que 65.
+*/
+
+/*var numAleatorio=Math.random()
+console.log(numAleatorio)
+if(numAleatorio>=0,5){
+    alert("Greater than 0,5")
+}else{
+    alert("lower tha 0,5")
+}
+*/
+
+/*var Age =65
+if(Age<18){
+    alert("Menor")
+}
+else{
+    if(Age>=18 && Age<65){
+        alert("Mayor")
+    }else{
+        alert("Jubilado")
+    }
+}*/
+
+
+
+
